036ca877839bf07b2d872d8aebad0302fc769f79
sjparkk <sjparkk0820@naver.com>
2023-05-22 22:01:11 +0900

    - git init

---
+++ src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sjpark.tobyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjpark/tobyspringboot/TobyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjpark.tobyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
