eb88c8415a6a02f167e86213fd8b863227121ef2
sjparkk <sjparkk0820@naver.com>
2023-08-02 22:52:26 +0900

    fix - 직접 구현을 위한 스프링부트 어노테이션 및 run 함수 삭제

--- src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
@@ -1,13 +1,8 @@
 package com.sjpark.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TobyspringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobyspringbootApplication.class, args);
     }
 
 }
