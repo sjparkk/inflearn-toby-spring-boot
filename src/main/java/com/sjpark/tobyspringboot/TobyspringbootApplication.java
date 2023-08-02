649c2b7eb0e76e4fb9a731c6c10e5dbf8b41142e
sjparkk <sjparkk0820@naver.com>
2023-08-02 22:53:08 +0900

    feat - 메인 메서드 동작 확인

--- src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
@@ -3,6 +3,7 @@ package com.sjpark.tobyspringboot;
 public class TobyspringbootApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello Containerless Standalone Application");
     }
 
 }
