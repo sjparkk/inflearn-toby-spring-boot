56712d90b16cf6066401c0632185c88b50a0cd7b
sjparkk <sjparkk0820@naver.com>
2023-08-02 22:44:50 +0900

    feat - Hello Controller 추가

---
+++ src/main/java/com/sjpark/tobyspringboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.sjpark.tobyspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+
+}
