3289a0e21bce393589e4eb3c8d3aff20ef23cae4
sjparkk <sjparkk0820@naver.com>
2023-08-08 21:49:15 +0900

    feat - 의존 오브젝트 추가

--- src/main/java/com/sjpark/tobyspringboot/HelloController.java
+++ src/main/java/com/sjpark/tobyspringboot/HelloController.java
@@ -1,8 +1,12 @@
 package com.sjpark.tobyspringboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 
 }
---
+++ src/main/java/com/sjpark/tobyspringboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package com.sjpark.tobyspringboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
