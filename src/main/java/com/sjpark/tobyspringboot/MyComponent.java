e19b6c55681cfc1ac42b18705e4c65f8a7ca3a14
sjparkk <sjparkk0820@naver.com>
2024-01-03 21:59:20 +0900

    feat - 메타 어노테이션을 이용한 커스텀 어노테이션으로 변경 (계층을 설명을 하거나 명확히 하고 싶을 때 이러한 커스텀 어노테이션 사용 - ex @Controller, @Service.. )

---
+++ src/main/java/com/sjpark/tobyspringboot/MyComponent.java
@@ -0,0 +1,14 @@
+package com.sjpark.tobyspringboot;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Component
+public @interface MyComponent {
+}
