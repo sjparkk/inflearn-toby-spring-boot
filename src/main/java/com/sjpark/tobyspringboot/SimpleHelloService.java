88461b1dfb80d7a8c04f9fda4467b1381c283352
sjparkk <sjparkk0820@naver.com>
2024-01-03 22:07:32 +0900

    feat - @Controller, @Service

--- src/main/java/com/sjpark/tobyspringboot/SimpleHelloService.java
+++ src/main/java/com/sjpark/tobyspringboot/SimpleHelloService.java
@@ -1,8 +1,10 @@
 package com.sjpark.tobyspringboot;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Component
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name) {
