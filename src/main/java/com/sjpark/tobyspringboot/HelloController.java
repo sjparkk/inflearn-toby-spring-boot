e7f0bb18c1c8caaf57dc49cba8445d9d14291eee
sjparkk <sjparkk0820@naver.com>
2024-01-29 22:31:49 +0900

    feat - setApplicationContext 호출 확인

--- src/main/java/com/sjpark/tobyspringboot/HelloController.java
+++ src/main/java/com/sjpark/tobyspringboot/HelloController.java
@@ -1,13 +1,17 @@
 package com.sjpark.tobyspringboot;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
 @RestController
-public class HelloController {
+public class HelloController implements ApplicationContextAware {
     private final HelloService helloService;
+    private ApplicationContext applicationContext;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
@@ -18,4 +22,10 @@ public class HelloController {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 
+    //스프링 컨테이너가 초기화되는 시점에 해당 작업이 일어남
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(applicationContext);
+        this.applicationContext = applicationContext;
+    }
 }
