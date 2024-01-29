186a6d12506b087f56f413a20d9e4cc6f658c019
sjparkk <sjparkk0820@naver.com>
2024-01-29 22:33:04 +0900

    feat - 생성자 주입으로 변경

--- src/main/java/com/sjpark/tobyspringboot/HelloController.java
+++ src/main/java/com/sjpark/tobyspringboot/HelloController.java
@@ -9,12 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Objects;
 
 @RestController
-public class HelloController implements ApplicationContextAware {
+public class HelloController {
     private final HelloService helloService;
-    private ApplicationContext applicationContext;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
+
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
     }
 
     @GetMapping("/hello")
@@ -22,10 +26,6 @@ public class HelloController implements ApplicationContextAware {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 
-    //스프링 컨테이너가 초기화되는 시점에 해당 작업이 일어남
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        System.out.println(applicationContext);
-        this.applicationContext = applicationContext;
-    }
+
+
 }
