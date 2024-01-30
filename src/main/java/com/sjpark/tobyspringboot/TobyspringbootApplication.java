945c1493a087a3f5ac3f1cf62e8675b845cd5ee7
sjparkk <sjparkk0820@naver.com>
2024-01-30 22:58:21 +0900

    feat - SpringBootApplication 과 비교

--- src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
+++ src/main/java/com/sjpark/tobyspringboot/TobyspringbootApplication.java
@@ -1,12 +1,10 @@
 package com.sjpark.tobyspringboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -24,30 +22,10 @@ public class TobyspringbootApplication {
     }
 
     public static void main(String[] args) {
-
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-                ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                //없더라도 스프링 컨테이너가 주입을 해줘서 사용이 가능함.
-                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(TobyspringbootApplication.class);
-        applicationContext.refresh();
-
-        //http -v ":8080/hello?name=Spring"
-        //http -v POST ":8080/hello?name=Spring"
-
+        MySpringApplication.run(TobyspringbootApplication.class, args);
     }
 
+//    public static void main(String[] args) {
+//        SpringApplication.run(TobyspringbootApplication.class, args);
+//    }
 }
